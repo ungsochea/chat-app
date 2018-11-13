0969379c562a295eca2418cb3b785677b247c37e
ungsochea <sochea.ung@gmail.com>
2018-11-13 14:57:50 +0700

    Factory | Fake data create to database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        // factory(User::class,3)->create();
+        factory(User::class)->create([
+            'email'=>'info@chea.me',
+            'name'=>'chea' 
+        ]);
+
+        factory(User::class)->create([
+            'email'=>'admin@chea.me',
+            'name'=>'Admin' 
+        ]);
+
+        factory(User::class)->create([
+            'email'=>'user@chea.me',
+            'name'=>'User' 
+        ]);
     }
 }
