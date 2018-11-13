ad8e5df6cb79bf599da9e5ad5d49bfed7c0719ff
ungsochea <sochea.ung@gmail.com>
2018-11-13 16:17:37 +0700

    make Models / Controller / Resource

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use Illuminate\Foundation\Auth\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
+        
+        // return new UserResource(auth()->user());
+        return UserResource::collection(User::all());
         return view('home');
     }
 }
